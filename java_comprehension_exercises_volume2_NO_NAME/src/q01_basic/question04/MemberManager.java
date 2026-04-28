b0fec2d240a55812ef1fe55c75914ea740920e94
knaganu <knaganu@C010423431.zipangu.cac.co.jp>
2026-04-28 13:52:22 +0900

    basic.question01～04

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question04/MemberManager.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question04/MemberManager.java
@@ -1,13 +1,19 @@
 package q01_basic.question04;
 
-class MemberManager {
+class MemberManager extends Member {
 
 	/**
 	 * インスタンス化の禁止
 	 */
 	private MemberManager() {
+		//TODO showAllMembersメソッドを実装する
+
 	}
 
-	//TODO showAllMembersメソッドを実装する
+	public static void showAllMembers(Member[] members) {
+		for (Member member : members) {
+			member.showMember();
+		}
+	}
 
 }
